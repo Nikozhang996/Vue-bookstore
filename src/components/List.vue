70cff5e3b3dabd36532daf3cd64f10c8fc0f6afa
Niko_Zhang <593177876@qq.com>
2018-02-19 12:21:01 +0800

    下拉刷新，上拉加载更多

--- src/components/List.vue
+++ src/components/List.vue
@@ -43,33 +43,70 @@
         books: [],
         offset: 0,
         hasMore: true,
-        isLoading:false
+        isLoading: false
       };
     },
     created () {
       this._getAllBooks();
     },
-    mounted(){
+    mounted () {
       let scroll = this.$refs.scroll;
       let top = scroll.offsetTop;
-      scroll.addEventListener('touchstart',(ev)=>{
-        let start = ev.touches[0].pageY;
-        scroll.addEventListener('touchmove', (ev) => {
+      let distance = 0;
+      scroll.addEventListener('touchstart', (ev) => {
+        if (scroll.scrollTop != 0 || scroll.offsetTop != top) {
+          return
+        }
+        let start = ev.touches[0].pageY;  //手指点击的起始位置
+        let move = (ev) => {
           let current = ev.touches[0].pageY;
+          distance = current - start;
+          if (distance > 0) {
+            if (distance <= 50) {
+              scroll.style.top = distance + top + 'px';
+            } else {
+              distance = 50;
+              scroll.style.top = top + 50 + 'px'
+            }
+          } else {
+            // 如果不在考虑满园内，则移除move和end事件
+            scroll.removeEventListener('touchmove', move);
+            scroll.removeEventListener('touchend', end);
+          }
+        };
 
-        },false);
-        console.log(start);
-      },false);
+        let end = (ev) => {
+          clearInterval(this.scrollTimer);
+          this.scrollTimer = setInterval(() => {
+            if (distance <= 0) {
+              clearInterval(this.scrollTimer);
+              distance = 0;
+              scroll.style.top = top + 'px';
+              this.books = [];
+              this.books = this._getAllBooks();
+              this.offset = 0;
+              scroll.removeEventListener('touchmove', move);
+              scroll.removeEventListener('touchend', end);
+              return;
+            }
+            distance -= 1;
+            scroll.style.top = top + distance + 'px';
+          }, 10);
+        };
+
+        scroll.addEventListener('touchmove', move);
+        scroll.addEventListener('touchend', end);
+      }, false);
     },
     methods: {
-      moreBook(){
+      moreBook () {
         this._getAllBooks();
       },
       async _getAllBooks () {
-        if(this.hasMore && !this.isLoading) {   //防止用户多次点击改善多次请求
+        if (this.hasMore && !this.isLoading) {   //防止用户多次点击改善多次请求
           this.isLoading = true;
           let {hasMore, books} = await pagination(this.offset);
-          this.books = [...this.books,...books];
+          this.books = [...this.books, ...books];
           this.hasMore = hasMore;
           this.offset = this.books.length;
           this.isLoading = false;
@@ -79,18 +116,18 @@
         // await removeBook(id);  // 删除后台数据
         this.books = this.books.filter(item => item.bookId !== id);
       },
-      loadMore(){
+      loadMore () {
         /*
         * 此处需要函数节流思想，防止多次出发事件导致性能消耗
         * */
         clearTimeout(this.timer);
         this.timer = setTimeout(() => {
-          let{scrollTop,clientHeight,scrollHeight} = this.$refs.scroll;
-          if (scrollTop+clientHeight+20>scrollHeight) {
+          let {scrollTop, clientHeight, scrollHeight} = this.$refs.scroll;
+          if (scrollTop + clientHeight + 20 > scrollHeight) {
             this._getAllBooks();
           }
-          console.log(this.$refs.scroll.scrollTop);
-        },100);
+          // console.log(this.$refs.scroll.scrollTop);
+        }, 100);
       }
     }
   }
